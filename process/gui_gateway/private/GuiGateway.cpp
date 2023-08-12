6a5463c9ca79a70b196debc8d4646f7c6fcf53e1
Piotr Henczel <henczel.piotr@gmail.com>
2023-08-12 11:17:09 +0200

    Testing.

--- process/gui_gateway/private/GuiGateway.cpp
+++ process/gui_gateway/private/GuiGateway.cpp
@@ -45,18 +45,10 @@ void GuiGateway::runProcess(int argc, char * argv[])
 void GuiGateway::runOnGui()
 {
     // Testing puprposes only. Need to be rewrite
-    m_diagnostic_comm_handler = std::make_unique<InetCommHandler<DiagnosticData>>(
-        sizeof(odin::diagnostic_handler::DiagnosticData), DIAGNOSTIC_SOCKET_PORT);
-    m_diagnostic_shmem_handler = std::make_unique<ShmemHandler<DiagnosticData>>(
-        odin::shmem_wrapper::DataTypes::DIAGNOSTIC_FROM_REMOTE_SHMEM_NAME, sizeof(DiagnosticData), true);
-    m_control_selection_comm_handler = std::make_unique<InetCommHandler<OdinControlSelection>>(
-        sizeof(OdinControlSelection), CONTROL_SELECTION_PORT);
-    m_control_selection_shmem_handler = std::make_unique<ShmemHandler<OdinControlSelection>>(
-        odin::shmem_wrapper::DataTypes::CONTROL_SELECT_SHMEM_NAME, sizeof(OdinControlSelection), false);
-
     std::cout << "Assigning threads." << std::endl;
-
+    std::cout << "Starting GUI diagnostic thread." << std::endl;
     m_diagnostic_thread = std::thread(handleGuiDiagnostic, this);
+    std::cout << "Starting GUI control selection thread." << std::endl;
     m_control_selection_thread = std::thread(handleGuiControlSelection, this);
     m_diagnostic_thread.join();
     m_control_selection_thread.join();
@@ -65,20 +57,11 @@ void GuiGateway::runOnGui()
 void GuiGateway::runOnArm()
 {
     // Testing puprposes only. Need to be rewrite
-    m_diagnostic_comm_handler = std::make_unique<InetCommHandler<DiagnosticData>>(
-        sizeof(odin::diagnostic_handler::DiagnosticData), DIAGNOSTIC_SOCKET_PORT, ROBOTIC_GUI_IP);
-    m_diagnostic_shmem_handler = std::make_unique<ShmemHandler<DiagnosticData>>(
-        odin::shmem_wrapper::DataTypes::DIAGNOSTIC_SHMEM_NAME, sizeof(DiagnosticData), false);
-    m_control_selection_comm_handler = std::make_unique<InetCommHandler<OdinControlSelection>>(
-        sizeof(OdinControlSelection), CONTROL_SELECTION_PORT, ROBOTIC_GUI_IP);
-    m_control_selection_shmem_handler = std::make_unique<ShmemHandler<OdinControlSelection>>(
-        odin::shmem_wrapper::DataTypes::CONTROL_SELECT_SHMEM_NAME, sizeof(OdinControlSelection), true);
-
-    m_control_selection_shmem_handler->shmemWrite(&m_control_selection);
 
     std::cout << "Assigning threads." << std::endl;
-
+    std::cout << "Starting ARM diagnostic thread." << std::endl;
     m_diagnostic_thread = std::thread(handleArmDiagnostic, this);
+    std::cout << "Starting ARM control selection thread." << std::endl;
     m_control_selection_thread = std::thread(handleArmControlSelection, this);
     m_diagnostic_thread.join();
     m_control_selection_thread.join();
@@ -86,17 +69,21 @@ void GuiGateway::runOnArm()
 
 void GuiGateway::handleGuiDiagnostic(GuiGateway * gg)
 {
-    std::cout << "Starting GUI diagnostic thread." << std::endl;
+
+    gg->m_diagnostic_shmem_handler = std::make_unique<ShmemHandler<DiagnosticData>>(
+        odin::shmem_wrapper::DataTypes::DIAGNOSTIC_FROM_REMOTE_SHMEM_NAME, sizeof(DiagnosticData), true);
+    gg->m_diagnostic_comm_handler = std::make_unique<InetCommHandler<DiagnosticData>>(
+        sizeof(odin::diagnostic_handler::DiagnosticData), DIAGNOSTIC_SOCKET_PORT);
     while (gg->m_run_process)
     {
         // std::cout << "Reading data from server." << std::endl;
-        gg->m_diagnostic_comm_handler->serverRead(&gg->m_remote_diagnostic);
+        gg->m_diagnostic_comm_handler->serverRead(&(gg->m_remote_diagnostic));
         // std::cout << gg->m_remote_diagnostic.cpu_temp << " "
         //         << gg->m_remote_diagnostic.cpu_usage << " "
         //         << gg->m_remote_diagnostic.ram_usage << " "
         //         << gg->m_remote_diagnostic.latency << std::endl;
         // std::cout << "Writing to shmem." << std::endl;
-        gg->m_diagnostic_shmem_handler->shmemWrite(&gg->m_remote_diagnostic);
+        gg->m_diagnostic_shmem_handler->shmemWrite(&(gg->m_remote_diagnostic));
         // TODO: change magic number to settings constants for sleeps
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }
@@ -104,13 +91,18 @@ void GuiGateway::handleGuiDiagnostic(GuiGateway * gg)
 
 void GuiGateway::handleGuiControlSelection(GuiGateway * gg)
 {   
-    std::cout << "Starting GUI control selection thread." << std::endl;
+    gg->m_control_selection_shmem_handler = std::make_unique<ShmemHandler<OdinControlSelection>>(
+        odin::shmem_wrapper::DataTypes::CONTROL_SELECT_SHMEM_NAME, sizeof(OdinControlSelection), false);
+    gg->m_control_selection_comm_handler = std::make_unique<InetCommHandler<OdinControlSelection>>(
+        sizeof(OdinControlSelection), CONTROL_SELECTION_PORT);
     while (gg->m_run_process)
     {
         if (gg->m_control_selection_shmem_handler->openShmem())
         {
-            if(gg->m_control_selection_shmem_handler->shmemRead(&gg->m_control_selection))
-                gg->m_control_selection_comm_handler->serverWrite(&gg->m_control_selection);
+            if(gg->m_control_selection_shmem_handler->shmemRead(&(gg->m_control_selection)))
+            {
+                gg->m_control_selection_comm_handler->serverWrite(&(gg->m_control_selection));
+            }
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(2));
     }
@@ -118,12 +110,21 @@ void GuiGateway::handleGuiControlSelection(GuiGateway * gg)
 
 void GuiGateway::handleArmDiagnostic(GuiGateway * gg)
 {
-    std::cout << "Starting ARM diagnostic thread." << std::endl;
+    gg->m_diagnostic_shmem_handler = std::make_unique<ShmemHandler<DiagnosticData>>(
+        odin::shmem_wrapper::DataTypes::DIAGNOSTIC_SHMEM_NAME, sizeof(DiagnosticData), false);
+    gg->m_diagnostic_comm_handler = std::make_unique<InetCommHandler<DiagnosticData>>(
+        sizeof(odin::diagnostic_handler::DiagnosticData), DIAGNOSTIC_SOCKET_PORT, ROBOTIC_GUI_IP);
     while (gg->m_run_process)
     {
         // std::cout << "Reading from shmem." << std::endl;
-        if (gg->m_diagnostic_shmem_handler->shmemRead(&gg->m_remote_diagnostic))
-            gg->m_diagnostic_comm_handler->clientWrite(&gg->m_remote_diagnostic);
+        if (gg->m_diagnostic_shmem_handler->openShmem())
+        {
+            if (gg->m_diagnostic_shmem_handler->shmemRead(&(gg->m_remote_diagnostic)))
+            {
+                std::cout << "Writing diagnostic to server." << std::endl;
+                gg->m_diagnostic_comm_handler->clientWrite(&(gg->m_remote_diagnostic));
+            }
+        }
         // std::cout << gg->m_remote_diagnostic.cpu_temp << " "
         //     << gg->m_remote_diagnostic.cpu_usage << " "
         //     << gg->m_remote_diagnostic.ram_usage << " "
@@ -135,11 +136,16 @@ void GuiGateway::handleArmDiagnostic(GuiGateway * gg)
 
 void GuiGateway::handleArmControlSelection(GuiGateway * gg)
 {
-    std::cout << "Starting ARM control selection thread." << std::endl;
+    gg->m_control_selection_shmem_handler = std::make_unique<ShmemHandler<OdinControlSelection>>(
+        odin::shmem_wrapper::DataTypes::CONTROL_SELECT_SHMEM_NAME, sizeof(OdinControlSelection), true);
+    gg->m_control_selection_comm_handler = std::make_unique<InetCommHandler<OdinControlSelection>>(
+        sizeof(OdinControlSelection), CONTROL_SELECTION_PORT, ROBOTIC_GUI_IP);
+    gg->m_control_selection_shmem_handler->shmemWrite(&(gg->m_control_selection));
     while (gg->m_run_process)
     {
-        gg->m_control_selection_comm_handler->clientRead(&gg->m_control_selection);
-        gg->m_control_selection_shmem_handler->shmemWrite(&gg->m_control_selection);
+        std::cout << "Reading controlls from server." << std::endl;
+        gg->m_control_selection_comm_handler->clientRead(&(gg->m_control_selection));
+        gg->m_control_selection_shmem_handler->shmemWrite(&(gg->m_control_selection));
         std::this_thread::sleep_for(std::chrono::milliseconds(2));
     }
 }
